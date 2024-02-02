934f50a8db2a894c6c1b29a745285cc5a7cbf897
lenovo 03 <desaiharsh512@gmail.com>
2024-02-02 14:16:28 +0530

    An error is solved

--- app/src/main/java/com/example/exp_20_2_mad/MainActivity.java
+++ app/src/main/java/com/example/exp_20_2_mad/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    public void startService(){
+    public void startService(View v){
         startService(new Intent(this, MyService.class));
     }
 
-    public void stopService(){
+    public void stopService(View v){
         stopService(new Intent(this, MyService.class));
     }
 }
